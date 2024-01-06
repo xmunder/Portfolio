c6539fbfed4e8f22fec19b6a699f40c535ae5fdd
Cristian Gómez <gomezcristian676@gmail.com>
2024-01-06 16:22:37 -0500

    Agrego proyectos, arreglo el responsive en resume y cambio el favicon

--- src/components/showProjects.js
+++ src/components/showProjects.js
@@ -2,43 +2,69 @@ const projects = [
   {
     id: ["All", "Electronic"],
     name: "Buck Converter",
+    img: "/assets/img/buck.png",
+    alt: "Buck Converter manual",
     github_url: null,
+    link_url: null,
     drive_url:
       "https://drive.google.com/file/d/16yGnnstRwqyekB-gg0_EN5ROPei8SHt6/view?usp=sharing",
     drive_icon: "/assets/icons/drive.svg",
-    img: "/assets/img/buck.png",
-    alt: "Buck Converter manual",
   },
   {
     id: ["All", "Electronic"],
     name: "IoT-USCO",
+    img: "/assets/img/iot.png",
+    alt: "IoT Platform and Hardware",
+    link_url: null,
     github_url: "https://github.com/xmunder/IoT-Usco-Project",
     github_icon: "/assets/icons/github.svg",
     drive_url:
       "https://drive.google.com/file/d/1mlF23dkRVumWdiayWZ8pxzhu-qmB-FIJ/view?usp=sharing",
     drive_icon: "/assets/icons/drive.svg",
-    img: "/assets/img/iot.png",
-    alt: "IoT Platform and Hardware",
   },
   {
     id: ["All", "Electronic"],
     name: "PID Auto-Tuning",
+    img: "/assets/img/ahkr.png",
+    alt: "Auto-Tuning Controllers AH-KR",
     github_url: null,
+    link_url: null,
     drive_url:
       "https://drive.google.com/file/d/1inAgj18hQT2TenAvGqxq8SSo4OA7SmAR/view?usp=sharing",
     drive_icon: "/assets/icons/drive.svg",
-    img: "/assets/img/ahkr.png",
-    alt: "Auto-Tuning Controllers AH-KR",
   },
   {
     id: ["All", "Electronic"],
     name: "Real-time monitoring",
+    img: "/assets/img/labview.png",
+    alt: "Real-time monitoring with LabVIEW",
     github_url: null,
+    link_url: null,
     drive_url:
       "https://drive.google.com/file/d/18A53tz3AW2l8oq3h2RRh3HY_YfTrq1hm/view?usp=sharing",
     drive_icon: "/assets/icons/drive.svg",
-    img: "/assets/img/labview.png",
-    alt: "Real-time monitoring with LabVIEW",
+  },
+  {
+    id: ["All", "Web Development"],
+    name: "Moképon Game",
+    img: "/assets/img/mokepon.png",
+    alt: "Moképon Game",
+    drive_url: null,
+    github_url: "https://github.com/xmunder/MokeponGame",
+    github_icon: "/assets/icons/github.svg",
+    link_icon: "/assets/icons/link.svg",
+    link_url: "https://play-mokepon.netlify.app/",
+  },
+  {
+    id: ["All", "Web Development"],
+    name: "Calculator App",
+    img: "/assets/img/calculator.png",
+    alt: "Calculator APP",
+    drive_url: null,
+    github_url: "https://github.com/xmunder/calculatorApp",
+    github_icon: "/assets/icons/github.svg",
+    link_icon: "/assets/icons/link.svg",
+    link_url: "https://app-calculatorjs.netlify.app/",
   },
 ];
 
@@ -54,9 +80,9 @@ class ShowProjects extends HTMLElement {
       projectsToShow.map((element) => {
         projectsContainer.innerHTML += `
             <div class="bg-gradient-to-r from-onyx via-jet to-smoky-black mb-2 p-4 flex flex-col border-[1.25px] border-jet rounded-3xl w-full">
-            <img class="w-full h-[125px]" src="${element.img}" alt="${
-          element.alt
-        }">
+            <img class="w-full h-[125px] rounded-xl" src="${
+              element.img
+            }" alt="${element.alt}">
             <div class="flex flex-col">
             <h3 class="font-semibold text-[14px] mt-4">${element.name}</h3>
             <div class="flex flex-row justify-between mt-1">
@@ -72,6 +98,11 @@ class ShowProjects extends HTMLElement {
                     ${
                       element.drive_url !== null
                         ? `<a href="${element.drive_url}" target="_blank"><img class="w-5 h-5" src="${element.drive_icon}" alt="Drive Icon"></a>`
+                        : ``
+                    }
+                    ${
+                      element.link_url !== null
+                        ? `<a href="${element.link_url}" target="_blank"><img class="w-5 h-5" src="${element.link_icon}" alt="URL Icon"></a>`
                         : ``
                     }
                 </div>
